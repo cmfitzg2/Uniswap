fea39fa17d4bcc39d5354979964ca723faa80628
Collin Fitzgerald <cmfitzg2@ncsu.edu>
2021-04-14 22:49:59 -0400

    added polling for max price, split private key to untracked file

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
 const { ChainId, Fetcher, WETH, Route, Pair, Trade, Token, TokenAmount, TradeType, Percent, FACTORY_ADDRESS, INIT_CODE_HASH } = require('@uniswap/sdk');
-const { pack, keccak256 } = require('@ethersproject/solidity');
-const { getCreate2Address } = require('@ethersproject/address');
-
-
+const { InfuraProvider } = require('@ethersproject/providers');
+const fs = require('fs');
 const ethers = require('ethers');
 const yesno = require('yesno');
 
 const chainId = ChainId.MAINNET;
 const gweiMultiplier = 1e9;
 const gasFee = 90;
-//input = USDC, output = GRT
 const inputTokenAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 const inputTokenSymbol = 'USDC';
 const desiredInputAmount = 50;
-const outputTokenAddress = '0xc55c2175E90A46602fD42e931f62B3Acc1A013Ca';
-const outputTokenSymbol = 'FAKE';
+const outputTokenAddress = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599';
+const outputTokenSymbol = 'BTC';
 const creditAddress = '0x713cC41082d2f2446f66AE69860eF98D172b64CB';
 const expirationTime = Math.floor(Date.now() / 1000) + 60 * 30;
-const privKey = 'GOES HERE'
-const uniswapRouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
-const init = async () => {
-	const outputToken = await Fetcher.fetchTokenData(chainId, outputTokenAddress);
-	const inputToken = await Fetcher.fetchTokenData(chainId, inputTokenAddress);
+let data = JSON.parse(fs.readFileSync('secret.json'));
+const privKey = data.PRIVATE_KEY;
+const uniswapRouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+const maxPriceWithoutSlippage = 63080;
+const provider = new InfuraProvider("homestead", {
+    projectId: 'f49e6c05d6814347a848da44864f8e9d',
+    projectSecret: data.INFURA_KEY
+});
+
+const waitAvailability = async (skipOutput) => {
+	const outputToken = await Fetcher.fetchTokenData(chainId, outputTokenAddress, provider);
+	const inputToken = await Fetcher.fetchTokenData(chainId, inputTokenAddress, provider);
 	const desiredInputAmountAdjusted = (desiredInputAmount * (10 ** inputToken.decimals)).toString();
 	const weth = WETH[chainId];
-	const inputTokenEthPair = await Fetcher.fetchPairData(inputToken, weth);
-	let ready = false;
+	const inputTokenEthPair = await Fetcher.fetchPairData(inputToken, weth, provider);
 	try {
-		await Fetcher.fetchPairData(outputToken, weth)
-		console.log("Available");
-		ready = true;
+		await Fetcher.fetchPairData(outputToken, weth, provider)
+		if (!skipOutput) {
+			console.log("Available");
+		}
+		attemptTrade(outputToken, inputToken, desiredInputAmountAdjusted, weth, inputTokenEthPair);
 	} catch (error) {
 		console.log("Not available yet");
-		sleep(1000);
-		init();
+		sleep(50);
+		waitAvailability(false);
 	}
-	if (ready) {
-		const ethOutputTokenPair = await Fetcher.fetchPairData(outputToken, weth)
-		const route = new Route([inputTokenEthPair, ethOutputTokenPair], inputToken)
-		try {
-			const trade = new Trade(route, new TokenAmount(inputToken, desiredInputAmountAdjusted), TradeType.EXACT_INPUT)
+}
+const attemptTrade = async (outputToken, inputToken, desiredInputAmountAdjusted, weth, inputTokenEthPair) => {
+
+	const ethOutputTokenPair = await Fetcher.fetchPairData(outputToken, weth, provider)
+	const route = new Route([inputTokenEthPair, ethOutputTokenPair], inputToken)
+	try {
+		const trade = new Trade(route, new TokenAmount(inputToken, desiredInputAmountAdjusted), TradeType.EXACT_INPUT)
+		if (trade.executionPrice.invert().toSignificant(6) > maxPriceWithoutSlippage) {
+			console.log("Current market price (" + trade.executionPrice.invert().toSignificant(6) + ") exceeds max threshold (" + maxPriceWithoutSlippage.toPrecision(6) + ")");
+			sleep(50);
+			waitAvailability(true);
+		} else {
 			console.log("Sending " + trade.inputAmount.toSignificant(6) + " " + inputTokenSymbol)
 			console.log("Receiving " + trade.outputAmount.toSignificant(6) + " " + outputTokenSymbol)
-			console.log("Execution price: " + trade.executionPrice.toSignificant(6) + " " + inputTokenSymbol)
-			console.log("Mid price: " + trade.nextMidPrice.toSignificant(6))
-			
+			console.log("Execution price: " + trade.executionPrice.invert().toSignificant(6) + " " + inputTokenSymbol)
+			console.log("Mid price: " + trade.nextMidPrice.invert().toSignificant(6))
 			const slippageTolerance = new Percent('50', '10000'); //50 bips
 			const amountOutGuaranteed = trade.minimumAmountOut(slippageTolerance).raw;
 			console.log("Minimum amount out: " + amountOutGuaranteed / (10 ** outputToken.decimals))
@@ -55,9 +66,6 @@ const init = async () => {
 			const value = trade.inputAmount.raw;
 			const amountIn = ethers.BigNumber.from(value.toString()).toHexString(); 
 			const amountOutMin = ethers.BigNumber.from(amountOutGuaranteed.toString()).toHexString();
-			const provider = ethers.getDefaultProvider('mainnet', {
-				infura: 'https://mainnet.infura.io/v3/f49e6c05d6814347a848da44864f8e9d'
-			});
 			//let gasPrice = await provider.getGasPrice();
 			console.log("Gas Price: " + gasFee + " gwei");
 			let gasPrice = '0x' + ((gasFee * gweiMultiplier).toString(16))
@@ -95,9 +103,9 @@ const init = async () => {
 					console.log('Transaction was mined in block ' + receipt.blockNumber);
 				}
 			}
-		} catch (error) {
-			console.error(error)
 		}
+	} catch (error) {
+		console.error(error)
 	}
 }
 
@@ -109,4 +117,4 @@ function sleep(milliseconds) {
   } while (currentDate - date < milliseconds);
 }
 
-init();
+waitAvailability(false);
